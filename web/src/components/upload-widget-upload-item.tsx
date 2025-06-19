281a29c33af711211af092d0d5d6d9946ce312f1
Tiago Lopes <tiagoltavares2002@gmail.com>
2025-06-18 21:36:26 -0300

    feature: criação do componente de dropzone

---
+++ web/src/components/upload-widget-upload-item.tsx
@@ -0,0 +1,53 @@
+import * as Progress from '@radix-ui/react-progress'
+import { Download, ImageUp, Link2, RefreshCcw, XIcon } from 'lucide-react'
+import { Button } from './ui/button'
+
+export function UploadWidgetUploadItem() {
+  return (
+    <div className="p-3 rounded-lg flex flex-col gap-3 shadow-shape-content bg-white/2 relative overflow-hidden">
+      <div className="flex flex-col gap-1">
+        <span className="text-xs font-medium flex items-center gap-1">
+          <ImageUp className="size-3 text-zinc-300" strokeWidth={1.5} />
+          <span>screenshot.png</span>
+        </span>
+
+        <span className="text-xxs text-zinc-400 flex items-center gap-1.5">
+          <span className="line-through">3MB</span>
+          <div className="size-1 rounded-full bg-zinc-700" />
+          <span>
+            300KB
+            <span className="text-green-400 ml-1">-94%</span>
+          </span>
+          <div className="size-1 rounded-full bg-zinc-700" />
+          <span>45%</span>
+        </span>
+      </div>
+
+      <Progress.Root className="h-1 bg-zinc-800 rounded-full overflow-hidden">
+        <Progress.Indicator className="bg-indigo-500 h-1" style={{ width: '45%' }} />
+      </Progress.Root>
+
+      <div className="absolute top-2.5 right-2.5 flex items-center gap-1">
+        <Button size="icon-small">
+          <Download className="size-4" strokeWidth={1.5} />
+          <span className="sr-only">Download compressed image</span>
+        </Button>
+
+        <Button size="icon-small">
+          <Link2 className="size-4" strokeWidth={1.5} />
+          <span className="sr-only">Copy Remote URL</span>
+        </Button>
+
+        <Button size="icon-small">
+          <RefreshCcw className="size-4" strokeWidth={1.5} />
+          <span className="sr-only">Retry Upload</span>
+        </Button>
+
+        <Button size="icon-small">
+          <XIcon className="size-4" strokeWidth={1.5} />
+          <span className="sr-only">Cancel Upload</span>
+        </Button>
+      </div>
+    </div>
+  )
+}
