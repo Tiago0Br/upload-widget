b0510c743a321c46d626dcef0245149b71cdc9cb
Tiago Lopes <tiagoltavares2002@gmail.com>
2025-06-20 18:27:53 -0300

    feature: implementando a funcionalidade de download da imagem

--- web/src/components/upload-widget-upload-item.tsx
+++ web/src/components/upload-widget-upload-item.tsx
@@ -37,8 +37,17 @@ export function UploadWidgetUploadItem({ uploadId, upload }: UploadWigetUploadIt
           <span className="line-through">{formatBytes(upload.originalSizeInBytes)}</span>
           <div className="size-1 rounded-full bg-zinc-700" />
           <span>
-            300KB
-            <span className="text-green-400 ml-1">-94%</span>
+            {formatBytes(upload.compressedSizeInBytes ?? 0)}
+            {upload.compressedSizeInBytes && (
+              <span className="text-green-400 ml-1">
+                -
+                {Math.round(
+                  ((upload.originalSizeInBytes - upload.compressedSizeInBytes) * 100) /
+                    upload.originalSizeInBytes
+                )}
+                %
+              </span>
+            )}
           </span>
           <div className="size-1 rounded-full bg-zinc-700" />
           {upload.status === 'success' && <span>100%</span>}
@@ -62,9 +71,11 @@ export function UploadWidgetUploadItem({ uploadId, upload }: UploadWigetUploadIt
       </Progress.Root>
 
       <div className="absolute top-2.5 right-2.5 flex items-center gap-1">
-        <Button size="icon-small" disabled={upload.status !== 'success'}>
-          <Download className="size-4" strokeWidth={1.5} />
-          <span className="sr-only">Download compressed image</span>
+        <Button size="icon-small" aria-disabled={upload.status !== 'success'} asChild>
+          <a href={upload.remoteUrl} download={upload.name}>
+            <Download className="size-4" strokeWidth={1.5} />
+            <span className="sr-only">Download compressed image</span>
+          </a>
         </Button>
 
         <Button
